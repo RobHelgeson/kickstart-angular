150ac76a5165e115d477a33ca00868cf7e6051d9
Rob Helgeson <robhelgeson@hotmail.com>
2026-04-23 15:38:01 -0400

    chore: update environment configuration for Contentstack integration
    - Set production and development environment variables for Contentstack API keys, tokens, and hosts.
    - Enable preview mode and specify the region for both production and development environments.

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   contentstack: {
-    apiKey: '',
-    deliveryToken: '',
-    previewToken: '',
-    environment: '',
-    region: '',
-    preview: false,
+    apiKey: 'bltd3dc7fd332d67ddf',
+    deliveryToken: 'cs09dbd6542e170a0141b18aef',
+    previewToken: 'csc28955dd48cd917f15eba169',
+    environment: 'preview',
+    region: 'gcp-na',
+    preview: true,
 
-    contentDelivery: '',
-    previewHost: '',
-    applicationHost: ''
+    contentDelivery: 'gcp-na-cdn.contentstack.com',
+    previewHost: 'gcp-na-rest-preview.contentstack.com',
+    applicationHost: 'gcp-na-app.contentstack.com'
   }
 };
